cf63637c39f6127fbfe1eba5f86bdb5a12db0c1b
Jiaming <hfter789@gmail.com>
2017-06-01 21:51:17 -0700

    fix intermittent error

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require('./server.babel');
 var express = require('express');
 var graphQLHTTP = require('express-graphql');
 var axios = require('axios');
+var log = require('winston').log;
 var Schema = require('./src/data/schema').Schema;
 var app = express();
 var initDevClient = require('./client.dev').default;
 var serveStatic = require('serve-static');
 const localCache = {};
-const isDev = false;
-// const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === 'development';
 
 if (isDev) {
   initDevClient();
@@ -18,17 +18,20 @@ function getSessionData(req, resp, next) {
   var { authorization: authToken } = req.headers;
   if (localCache[authToken]) {
     req.userId = localCache[authToken];
+    log('info', `Request from ${localCache[authToken]}(Cached)`);
     next();
+    return;
   }
-  return axios.get(`https://graph.facebook.com/me?access_token=${authToken}`)
+  axios.get(`https://graph.facebook.com/me?access_token=${authToken}`)
   .then(authResp => {
     const userId = authResp.data.id;
     localCache[authToken] = userId;
     req.userId = userId;
+    log('info', `Request from ${userId}`);
     next();
   })
   .catch(error => {
-    // throw error;
+    log('error', error);
     next();
   });
 }
